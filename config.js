6e432da5556719d2c3dd684201657e9a076fb4bb
Haruto <152605575+Haruto1711@users.noreply.github.com>
2024-06-11 23:28:56 +0700

    commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Siyuan 🥰';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100009706417404';
+const eventName = 'For My Baby';
+const titleCard = 'Love You Most in This Universe';
+const contentCard = "My Love, i spent my entire life wishing for someone like you, don't even think for a second that i would give you up, not today, not tomorrow, not for the rest of our lifes, you aren't just my wish come true, you are my beautiful reality, and i can't imagine my world without you, thank you for always being there for me, thank you for always standing up for me, thank you for listening to my problems, thank you for always hearing me out even noone cares about me, thank you for being such a wonderful person to me, i will always love you and forever";
+
+
+const giftImage = 'image.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
